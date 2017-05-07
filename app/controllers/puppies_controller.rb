140cd6121daf9fbcacc245d8ba70e9d1b6a4b63b
emekdahl <emily.ekdahl@gmail.com>
2017-05-07 10:45:05 -0500

    add puppies index action

---
+++ app/controllers/puppies_controller.rb
@@ -0,0 +1,5 @@
+class PuppiesController < ApplicationController
+  def index
+    render "index.html.erb"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+
 Rails.application.routes.draw do
   get "/products" => "products#index"
+  get "/puppies" => "puppies#index"
 end
